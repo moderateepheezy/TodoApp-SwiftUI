75dec112e8c518c610de58f7688dbc6e97b5c20f
Afees Lawal <moderateepheezy@gmail.com>
2020-08-04 19:54:33 +0100

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Afees Lawal on 04/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
